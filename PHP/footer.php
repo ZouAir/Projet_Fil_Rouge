e6f576fdf474a390445a520297790031811ee49d
POR-MNS-067\Stagiaire <reghaizouhair@gmail.com>
2026-05-26 14:51:50 +0200

    add footer.php & header.php & css files

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,6 +1,20 @@
-FOOTER :
-
-Infos du club (adresse, contact) ?
-Copyright ?
-Liens utiles (mentions légales, RGPD, etc) ?
-Réseaux sociaux ?
+<footer>
+    <div class="footer">
+        <div class="logo">
+            <a href="index2.html">
+                <img src="image/logo_noir.png" alt="logo"></a>
+        </div>
+        <div class="mentions container">
+            <p>© 2026. MNS Football Club - Tous droits réservés - <a target="_blank"
+                    href="mentionslegales.html">Mentions Légales</a></p>
+        </div>
+        <div class="reseaux">
+            <a target="_blank" href="https://www.facebook.com/">
+                <img src="image/icones/facebookNB.png" alt="logo facebook">
+            </a>
+            <a target="_blank" href="https://www.instagram.com/">
+                <img src="image/icones/instagramNB.png" alt="logo instagram">
+            </a>
+        </div>
+    </div>
+</footer>
