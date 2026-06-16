13e66d0b7a8a3c7a837b75b6edff0b2d15342ac3
POR-MNS-067\Stagiaire <reghaizouhair@gmail.com>
2026-06-16 09:54:08 +0200

    add deploiement files

--- PHP/bdd.php
+++ PHP/bdd.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $pdo = new PDO("mysql:host=mysql_8;dbname=ACA;charset=utf8", "root", "rootpassword");
+    $pdo = new PDO("mysql:host=172.21.0.5;dbname=sql_dw2025_zouhair2_stagiairesmns_fr;charset=utf8", "sql_dw2025_zouhair2_stagiairesmns_fr", "7a7eb856234b7");
     return $pdo;
 } catch (PDOException $e) {
     die("Erreur : " . $e->getMessage());
