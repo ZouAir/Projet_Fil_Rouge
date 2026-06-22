fe9191195c2d5eefc864121cf3ab0c496cfebb28
POR-MNS-067\Stagiaire <reghaizouhair@gmail.com>
2026-06-22 12:27:26 +0200

    update header & index

--- PHP/index.php
+++ PHP/index.php
@@ -34,7 +34,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/variables.css">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css" rel="stylesheet">
     <script src="assets/js/script.js" defer></script>
     <title>Login</title>
