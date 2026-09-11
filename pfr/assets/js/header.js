44067cd84269aca3f5dd0cb4d3498e16452f32a3
POR-MNS-067\Stagiaire <reghaizouhair@gmail.com>
2026-09-11 16:08:20 +0200

    finish burger btn

--- pfr/assets/js/header.js
+++ pfr/assets/js/header.js
@@ -1,6 +1,11 @@
 const burger = document.getElementById('burger-btn');
 const nav = document.querySelector('nav');
+const i = burger.querySelector('i');
+
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-open');
+    i.classList.toggle('bx-menu');
+    i.classList.toggle('bx-x');
 })
+
